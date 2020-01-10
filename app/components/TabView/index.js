94f0ae1a377d513f039d1309516c4480b2c42f5b
rncsungjin <knife331>
2020-01-10 16:37:49 +0900

    스크롤뷰 추가

--- app/components/TabView/index.js
+++ app/components/TabView/index.js
@@ -1,24 +1,60 @@
 import React, {useState} from 'react';
-import {Dimensions, View} from 'react-native';
+import {Dimensions, ScrollView, View} from 'react-native';
 import Tabs from '../Tabs';
 import {IS_X, NOTCH_HEIGHT, STATUS_BAR_HEIGHT} from '../../constants';
+const TabScore = () => {
+    return (
+        <ScrollView>
+            <View style={{height: 100, backgroundColor: '#ffabab'}}/>
+            <View style={{height: 100, backgroundColor: '#ffabfa'}}/>
+            <View style={{height: 100, backgroundColor: '#faffab'}}/>
+            <View style={{height: 100, backgroundColor: '#abab00'}}/>
+            <View style={{height: 100, backgroundColor: '#bbccff'}}/>
+            <View style={{height: 100, backgroundColor: '#ab00ff'}}/>
+            <View style={{height: 100, backgroundColor: '#ab1fff'}}/>
+        </ScrollView>
+    );
+};
+const TabColour = () => {
+    return (
+        <ScrollView>
+            <View style={{height: 200, backgroundColor: '#ffabfa'}}/>
+            <View style={{height: 200, backgroundColor: '#ab00ff'}}/>
+            <View style={{height: 200, backgroundColor: '#ab1fff'}}/>
+        </ScrollView>
+    );
+};
+const TabProduct = () => {
+    return (
+        <ScrollView>
+            <View style={{height: 100, backgroundColor: '#ffabab'}}/>
+            <View style={{height: 300, backgroundColor: '#ffabfa'}}/>
+            <View style={{height: 100, backgroundColor: '#bbccff'}}/>
+            <View style={{height: 150, backgroundColor: '#ab00ff'}}/>
+            <View style={{height: 200, backgroundColor: '#abff00'}}/>
+            <View style={{height: 200, backgroundColor: '#abff00'}}/>
+            <View style={{height: 200, backgroundColor: '#00ff00'}}/>
+            <View style={{height: 200, backgroundColor: '#ffff00'}}/>
+        </ScrollView>
+    );
+};
 const TabView = () => {
     const {width , height} = Dimensions.get('window');
     const [current, setCurrent] = useState(0);
     const getContent = () => {
-        let colour = '#ffabab';
+        let content = null;
         switch (current) {
             case 0:
-                colour = '#ababff';
+                content = <TabScore />;
                 break;
             case 1:
-                colour = '#abffab';
+                content = <TabColour />;
                 break;
             case 2:
-                colour = '#ffabab';
+                content = <TabProduct />;
         }
 
-        return colour;
+        return content;
     };
     const tabStyle = {
         width: width,
@@ -31,9 +67,7 @@ const TabView = () => {
                 menus={['SCORE', 'COLOR', 'PRODUCT']}
                 setCurrent={index => setCurrent(index)}
             />
-            <View
-                style={[tabStyle]}
-            />
+            {getContent()}
         </>
     );
 };
